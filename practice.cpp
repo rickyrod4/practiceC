21f865254bec8ddac2f98ce4dfe9efe7e8f278ea
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2021-01-29 12:24:48 -0600

    Sum for loop c++

--- practice.cpp
+++ practice.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main(){
 
-    cout << "Hello" << endl;
-    
+    int sum =0;
+
+    for(int i =0; i <= 100; i++){
+        sum = sum + i;
+    }
+
+    cout << sum << endl;
+
 
     return 0;
 }
