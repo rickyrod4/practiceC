e6cef3e08ee427327e390b8fb72073861c0f927f
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2021-02-03 14:28:08 -0600

    New FIles

--- practice.cpp
+++ practice.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-
-void showTable(){
-    int array1[2][2];
-    array1[0][0] = 1;
-    cout << array1[0][0] << endl;
-
-}
-int main(){
-
-    int sum =0;
-
-    for(int i =0; i <= 100; i++){
-        if(i%5 == 0 && i%8 == 0){
-            sum = sum + i;
-        }
-    }
-
-    cout << sum << endl;
-    showTable();
-
-
-    return 0;
-}
-//This concludes the main function
